283245e90dd124ef15c8ba23da13b8e2cfe97108
gmt9120 <gmt9120@stern.nyu.edu>
2022-07-11 16:22:31 -0400

    Update my_script.py

--- my_script.py
+++ my_script.py
@@ -4,3 +4,10 @@ print("HELLO WORLD!")
 x = 2 + 2
 print(x)
 print("My name is Ellie!")
+def do_stuff(message,first_name,last_name):
+    print("'" + message + "', says " + first_name + " " + last_name)
+do_stuff("Hello there", "Ophelia", "Clark")
+def calc_area(length,height):
+    return length*height
+print(calc_area(2,4))
+print(type(None))
